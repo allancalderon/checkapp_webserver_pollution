c936a40ca4f11fbc32c22955bb71d66e828b721f
Allan Mendez <calderonallan@gmail.com>
2019-09-29 14:05:32 +0200

    Update mambiente URL to get real time pollution data

--- pollution/include/Constants.php
+++ pollution/include/Constants.php
@@ -2,5 +2,5 @@
 /**
  * Database sensors variables
  */
-define("URL_ESPMAD", "http://www.mambiente.munimadrid.es/opendata/horario.txt");
+define("URL_ESPMAD", "http://www.mambiente.madrid.es/opendata/horario.txt");
 ?>
